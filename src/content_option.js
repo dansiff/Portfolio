014d8b885efd8c08ec2d43d47ec0ebdac35a4455
Daniel Sandoval <30529097+dansiff@users.noreply.github.com>
2024-10-15 23:48:06 -0500

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,166 @@
+const logotext = "Dan";
+const meta = {
+    title: "Daniel Sandoval",
+    description: "I’m Daniel Sandoval data scientist _ Full stack devloper,currently working in Chicago",
+};
+
+const introdata = {
+    title: "I’m Daniel Sandoval",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Si Hablo Espanol",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "My journey from customer service assistance. I embarked with the birth of my first daugther. Starting with C# programs that resulted in 2 published games on googleplay.  This coupled with my local website development created pathways for me.        .",
+};
+const worktimeline = [{
+        jobtitle: "Mobile App developer",
+        where: "Self-Employed",
+        date: "2016",
+    },
+    {
+        jobtitle: "Software Engineer",
+        where: "Gazam",
+        date: "2023",
+    },
+    {
+        jobtitle: "Dev Ops Engineer",
+        where: "Blue Sky telepsych",
+        date: "2024",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "My design focuses on reactive modern design with a clean image. If you have a design in mind I will duplicate.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "My mobile game experience included a robust payment system with Googleplay API for charging via google wallet.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "I have completed Wordpress websites including a vast medical professional directory for blue sky telepsych.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+    description: "Building the future, one line of code at a time.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Technology is the canvas; code is my paint.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Where challenges meet creativity, solutions are born.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Every great solution begins with a simple idea.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "Innovation isn't just about thinking outside the box; it's about reshaping the box itself.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Empowering businesses through technology that scales.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "danielsand711@gmail.com",
+    YOUR_FONE: "()-",
+    description: "Please contact us for any quotes on Web or app development. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
